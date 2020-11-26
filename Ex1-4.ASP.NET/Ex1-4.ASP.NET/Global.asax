18335a625017cb9a95fa9a979de91483c36800fa
Climber <786514683@qq.com>
2020-11-26 23:32:36 +0800

    作业

---
+++ Ex1-4.ASP.NET/Ex1-4.ASP.NET/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ex1_4.ASP.NET.Global" Language="C#" %>
---
+++ Ex1-4.ASP.NET/Ex1-4.ASP.NET/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Ex1_4.ASP.NET.Startup))]
+namespace Ex1_4.ASP.NET
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
